93fbd09c39b054e694e33ea49bd75c67fd46b75d
mikelxk <mike.lxk@gmail.com>
2022-12-28 15:12:47 -0500

    chore: use std::boolalpha over tertiary for bool

--- print.hpp
+++ print.hpp
@@ -8,8 +8,8 @@
 #include <typeinfo>
 #include <utility>
 using namespace std;
-//overload for tuple
-//https://stackoverflow.com/questions/6245735/pretty-print-stdtuple
+// overload for tuple
+// https://stackoverflow.com/questions/6245735/pretty-print-stdtuple
 template <typename TupType, size_t... I>
 void tuple_print(const TupType &_tup, std::index_sequence<I...>, string delim);
 template <typename... T>
@@ -41,7 +41,7 @@ struct is_bitset : std::false_type {
 template <size_t sz>
 struct is_bitset<std::bitset<sz>> : std::true_type {
 };
-//overload for pairs
+// overload for pairs
 template <typename T1, typename T2>
 void print(const pair<T1, T2> &p, string delim)
 {
@@ -50,10 +50,9 @@ void print(const pair<T1, T2> &p, string delim)
 
 #if __cplusplus >= 201709L
 template <typename T>
-concept printable = requires(const T &a)
-{
-    cout << a;
-};
+concept printable = requires(const T &a) {
+                        cout << a;
+                    };
 template <typename T>
 concept arr = is_array_v<T>;
 template <typename T>
@@ -61,7 +60,8 @@ concept ptr = is_pointer_v<T>;
 template <typename T>
 concept boolean = is_same_v<T, bool>;
 template <typename T>
-concept directPrintable = printable<T> && !boolean<T> && !is_bitset<T>() && !is_array_v<T>;
+concept directPrintable = printable<T> && !
+boolean<T> && !is_bitset<T>() && !is_array_v<T>;
 template <typename T>
 concept normalObj = !(directPrintable<T> || boolean<T> || arr<T> || ranges::input_range<T> || ptr<T> || is_tuple<T>() || is_bitset<T>());
 template <size_t sz>
@@ -74,11 +74,11 @@ template <ranges::input_range rng>
 void print(const rng &ir);
 template <normalObj obj>
 void print(const obj &o);
-//overload for bitset
+// overload for bitset
 template <boolean B>
-void print(const B &b,string delim)
+void print(const B &b, string delim)
 {
-    cout << (b ? "true" : "false") << delim;
+    cout << std::boolalpha << b << delim;
 }
 template <size_t sz>
 void print(const bitset<sz> &bs, string delim)
